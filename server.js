e1dc3eb7f305414fc3391a29dbb6b9726a63cb4b
Katzuno <erikhenning98@hotmail.com>
2018-12-17 11:06:59 +0000

    test initializare

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var app = express();
+
+app.use(express.static('public'));
+public_html = __dirname + "/public/";
+
+app.get('/', function (req, res) {
+    res.sendFile( public_html + "index.html" );
+ });
+
+ // MAI SUS NU SCHIMBI NIMIC
+// MAI JOS SCRII COD - ai 2 modele pentru request
+
+ app.get('/test', function(req, res)    {
+    res.send("Hello TEST");
+ });
+
+// Model POST request - pasul 3 acceptare cost
+app.post('/accept-cost', urlencodedParser, function (req, res) {
+    response = {
+        confirmat:req.body.confirmat,//Preiau atributul name din input
+        last_name:req.body.date_end
+    }
+    console.log("Got a POST request for the homepage");
+    //res.send('Hello POST');
+    res.send(response);
+});
+
+
+
+ // MAI JOS NU SCHIMBI NIMIC
+ var server = app.listen(1337, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("NodeJS app listening at http://157.230.24.127/app/");
+ });
